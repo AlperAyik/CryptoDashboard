2267a35962f537c76df9cd82d47730659452daf2
AlperAyik <alperayik06@gmail.com>
2026-06-19 01:38:27 +0200

    chart button click change made

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-// import Chart from 'chart.js/auto'
 import trending from './components/trending.ts' // currently treding cyrpto coins
+import Chart from 'chart.js/auto'
 
 const trendingCoins = document.getElementById("trending_coins") as HTMLDivElement;
 const trendingNft = document.getElementById("trending_nft") as HTMLDivElement;
@@ -13,18 +13,27 @@ async function trendingElements() {
     coins.forEach((coin = {}) => {
         let newDiv = document.createElement("div");
         newDiv.innerHTML = `
-        <h1>${coin.item.name}</h1>
-        <img src="${coin.item.thumb}" alt="">
-        `
-
+            <!-- fix align issue-->
+           <div class="flex">
+            <div class="grid grid-cols-1 p-2">
+                <h1 class="text-white">${coin.item.name}</h1>
+                <img src="${coin.item.small}" alt="">
+            </div>
+            <div class="flex justify-center items-center p-10 gap-2">
+                <h2 class="text-white">$${coin.item.data.price}</h2>
+                <h2 class="${coin.item.data.price_change_percentage_24h.usd > 1 ? "text-green-400" : "text-red-400"}">${Math.floor(100 * coin.item.data.price_change_percentage_24h.usd) / 100}%</h2>
+            </div>
+           </div>
+      `
         trendingCoins.appendChild(newDiv);
     })
 
-    nfts.forEach((coin = {}) => {
+    nfts.forEach((nft = {}) => {
         let newDiv = document.createElement("div");
         newDiv.innerHTML = `
-        <h1>${coin.name}</h1>
-        <img src="${coin.thumb}" alt="">
+        <h1 class="text-white">${nft.name}</h1>
+        <img src="${nft.thumb}" alt="">
+        <h2 class="${nft.floor_price_24h_percentage_change > 1 ? "text-green-400" : "text-red-400"}">${Math.floor(100 * nft.floor_price_24h_percentage_change) / 100}%</h2>
         `
 
         trendingNft.appendChild(newDiv);
@@ -32,24 +41,75 @@ async function trendingElements() {
 }
 
 trendingElements()
-// (async function() {
-//     const crypto = await getCoins();
-//     // const sorted = crypto.sort((a = any, b = any) => a.current_price - b.current_price);
-//     const data = crypto.coins;
-//
-//     new Chart(
-//         document.getElementById('acquisitions') as HTMLCanvasElement,
-//         {
-//             type: 'line',
-//             data: {
-//                 labels: data.map((row = any) => row.item.name),
-//                 datasets: [
-//                     {
-//                         label: 'Trending rank',
-//                         data: data.map((row = any) => row.item.score),
-//                     }
-//                 ]
-//             }
-//         }
-//     );
-// })();
+
+//test
+let myChart: Chart | null = null;
+const coinsBtn = document.getElementById('coinsBtn') as HTMLButtonElement;
+const nftsBtn = document.getElementById('nftsBtn') as HTMLButtonElement;
+coinsBtn.addEventListener('click', async() => {
+    if (myChart) {
+        myChart.destroy()
+        myChart = null
+    }
+
+   const result = await trending()
+
+    myChart =new Chart(
+        document.getElementById('acquisitions') as HTMLCanvasElement,
+        {
+            type: 'bar',
+            data: {
+                labels: result.coins.map((row: any) => row.item.name),
+                datasets: [{
+                    label: 'current % in 24h Coins',
+                    data: result.coins.map((row: any) => row.item.data.price_change_percentage_24h.usd),
+                }]
+            }
+        }
+    )
+})
+
+nftsBtn.addEventListener('click', async() => {
+    if (myChart) {
+        myChart.destroy()
+        myChart = null
+    }
+
+    const result = await trending()
+
+
+    myChart = new Chart(
+        document.getElementById('acquisitions') as HTMLCanvasElement,
+        {
+            type: 'bar',
+            data: {
+                labels: result.nfts.map((row: any) => row.name),
+                datasets: [{
+                    label: 'current % in 24h Nfts',
+                    data: result.nfts.map((row: any) => row.floor_price_24h_percentage_change),
+                }]
+            }
+        }
+    )
+})
+
+
+async function temp() {
+    const result = await trending()
+
+    myChart =new Chart(
+        document.getElementById('acquisitions') as HTMLCanvasElement,
+        {
+            type: 'bar',
+            data: {
+                labels: result.coins.map((row: any) => row.item.name),
+                datasets: [{
+                    label: 'current % in 24h Coins',
+                    data: result.coins.map((row: any) => row.item.data.price_change_percentage_24h.usd),
+                }]
+            }
+        }
+    )
+}
+
+temp();
